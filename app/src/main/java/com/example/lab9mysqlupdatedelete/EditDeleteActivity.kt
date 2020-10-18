25d75b64f16e6b7752e5f37d566e4d6656c851bc
supawadeeyamput <69790215+supawadeeyamput@users.noreply.github.com>
2020-10-18 23:28:34 +0700

    Add files via upload

--- app/src/main/java/com/example/lab9mysqlupdatedelete/EditDeleteActivity.kt
+++ app/src/main/java/com/example/lab9mysqlupdatedelete/EditDeleteActivity.kt
@@ -3,20 +3,21 @@ package com.example.lab9mysqlupdatedelete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_edit_delete.*
+import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 class EditDeleteActivity : AppCompatActivity() {
     val createClient = StudentAPI.create()
+    var mID: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_delete)
-
-        val mID = intent.getStringExtra("mId")
+        val mID = intent.getStringExtra("mId").toString()
         val mName = intent.getStringExtra("mName")
         val mAge = intent.getStringExtra("mAge")
 
@@ -25,33 +26,51 @@ class EditDeleteActivity : AppCompatActivity() {
         edt_name.setText(mName)
         edt_age.setText(mAge)
     }
-
-    fun saveStudent(v: View) {
+    fun saveStudent(view: View){
         createClient.updateStudent(
+
             edt_id.text.toString(),
             edt_name.text.toString(),
             edt_age.text.toString().toInt()
-        )
-            .enqueue(object : retrofit2.Callback<Student> {
-                override fun onResponse(
-                    call: retrofit2.Call<Student>,
-                    response: Response<Student>
-                ) {
-                    if (response.isSuccessful) {
-                        Toast.makeText(
-                            applicationContext,
-                            "Successfully Updated",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        finish()
-                    } else {
-                        Toast.makeText(applicationContext, "Update Failure", Toast.LENGTH_LONG)
-                            .show()
-                    }
+        ).enqueue(object : Callback<Student> {
+            override fun onResponse(call: Call<Student>, response: Response<Student>){
+                if (response.isSuccessful){
+                    Toast.makeText(applicationContext, "Successfully Updated", Toast.LENGTH_LONG).show()
+                    finish()
+                }else{
+                    Toast.makeText(applicationContext, "Update Failure", Toast.LENGTH_LONG).show()
                 }
+            }
+            override fun onFailure(call: Call<Student>, t: Throwable) = t.printStackTrace()
+        })
 
-                override fun onFailure(call: retrofit2.Call<Student>, t: Throwable) =
-                    t.printStackTrace()
-            })
+    }
+    fun deleteStudent(view: View){
+
+        val myBundle = AlertDialog.Builder(this)
+        myBundle.apply {
+            setTitle("Warning")
+            setMessage("Do you want to delete the student?")
+            setNegativeButton("Yes"){_,_->
+                createClient.deleteStudent(mID)
+                    .enqueue(object : Callback<Student>{
+                        override fun onResponse(call: Call<Student>, response: Response<Student>){
+                            if (response.isSuccessful){
+                                Toast.makeText(applicationContext, "Successfully Deleted", Toast.LENGTH_LONG).show()
+
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Student>, t: Throwable) {
+                            Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                        }
+                    })
+                finish()
+            }
+            setPositiveButton("No"){dialog, which -> dialog.cancel()
+            }
+            show()
+        }
+
     }
 }
--- app/src/main/java/com/example/lab9mysqlupdatedelete/MainActivity.kt
+++ app/src/main/java/com/example/lab9mysqlupdatedelete/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import android.telecom.Call
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import layout.EditStudentAdapter
 import retrofit2.Response
@@ -15,61 +17,55 @@ import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
     var studentList = arrayListOf<Student>()
-    var createClient = StudentAPI.create()
+    val createClient = StudentAPI.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.layoutManager = LinearLayoutManager(applicationContext)
-        recycler_view.addItemDecoration(DividerItemDecoration(recycler_view.context,
-            DividerItemDecoration.VERTICAL))
+        recycler_view.layoutManager = LinearLayoutManager(applicationContext) as RecyclerView.LayoutManager?
+        recycler_view.itemAnimator = DefaultItemAnimator() as RecyclerView.ItemAnimator?
+        recycler_view.addItemDecoration(DividerItemDecoration(recycler_view.getContext(), DividerItemDecoration.VERTICAL))
     }
 
     override fun onResume() {
         super.onResume()
-        callStudent()
+        callStudentData()
+    }
+    fun clickSearch(v:View){
+        studentList.clear();
+        if(edt_search.text.isEmpty()){
+            callStudentData()
+        }else{
+            createClient.retrieveStudentID(edt_search.text.toString())
+                .enqueue(object : retrofit2.Callback<Student> {
+
+                    override fun onResponse(call: retrofit2.Call<Student>, response: retrofit2.Response<Student>){
+                        studentList.add(Student(response.body()?.std_id.toString(),response.body()?.std_name.toString(),
+                            response.body()?.std_age.toString().toInt()))
+
+                        recycler_view.adapter = EditStudentAdapter(studentList,applicationContext)
+                    }
+                    override fun onFailure(call: retrofit2.Call<Student>, t: Throwable) = t.printStackTrace()
+                })
+        }
+
     }
 
-    fun callStudent(){
-        studentList.clear()
-        createClient.retrieveStudent()
+    fun callStudentData(){
+        studentList.clear();
+        createClient.
+        retrieveStudent()
             .enqueue(object : retrofit2.Callback<List<Student>> {
-                override fun onResponse(
-                    call: retrofit2.Call<List<Student>>,
-                    response: Response<List<Student>>
-                ) {
-                    response.body()?.forEach{
+                override fun onResponse(call: retrofit2.Call<List<Student>>, response: retrofit2.Response<List<Student>>) {
+                    response.body()?.forEach {
                         studentList.add(Student(it.std_id, it.std_name, it.std_age))
                     }
                     recycler_view.adapter = EditStudentAdapter(studentList, applicationContext)
                 }
 
-                override fun onFailure(call: retrofit2.Call<List<Student>>, t: Throwable) {
+                override fun onFailure(call: retrofit2.Call<List<Student>>, t: Throwable) =
                     t.printStackTrace()
-                    Toast.makeText(applicationContext, "Error2", Toast.LENGTH_LONG).show()
-                }
             })
-    }
 
-    fun clickSearch(v: View){
-        studentList.clear()
-        if(edt_search.text.isEmpty()){
-            callStudent()
-        }else{
-            createClient.retrieveStudentID(edt_search.text.toString())
-                .enqueue(object : retrofit2.Callback<Student> {
-                    override fun onResponse(call: retrofit2.Call<Student>, response: Response<Student>){
-                        if(response.isSuccessful){
-                            studentList.add(Student(response.body()?.std_id.toString(),
-                                response.body()?.std_name.toString(),
-                                response.body()?.std_age.toString().toInt()
-                            ))
-                            recycler_view.adapter= EditStudentAdapter(studentList,applicationContext)
-                        }
-                    }
-
-                    override fun onFailure(call: retrofit2.Call<Student>, t: Throwable) = t.printStackTrace()
-                })
-        }
     }
 }
